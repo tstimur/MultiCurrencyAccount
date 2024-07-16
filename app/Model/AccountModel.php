bc88eb3d3c25d083302d0f3efa0236f26f735b40
tstimur <tstimurb0311@gmail.com>
2024-07-17 01:44:44 +0700

    Feat: class AccountModel       Добавлены метод пополнения/списания средств, получение баланса, конвертации валюты, отключения валюты с дальнейшей конвертацией в основную валюту счета.

--- app/Model/AccountModel.php
+++ app/Model/AccountModel.php
@@ -48,5 +48,111 @@ class AccountModel
         return false;
     }
 
+    /**
+     * @param string $currencyCode
+     * @param float $amount
+     * @return bool
+     * Пополнение средств
+     * Проверка суммы пополнения на отрицательные значения в контроллере перед передачей в метод deposit()
+     */
+    public function deposit(string $currencyCode, float $amount): bool
+    {
+        if ($amount < 0) {
+            return false;
+        }
+        if (isset($this->balances[$currencyCode])) {
+            $this->balances[$currencyCode] += $amount;
+            return true;
+        }
+        return false;
+    }
 
+    /**
+     * @param string $currencyCode
+     * @param float $amount
+     * @return float|bool
+     * Списание средств
+     */
+    public function withdraw(string $currencyCode, float $amount): float|bool
+    {
+        if (!isset($this->balances[$currencyCode])) {
+            return false;
+        }
+        if ($amount > $this->balances[$currencyCode]) {
+            return false;
+        }
+        $this->balances[$currencyCode] -= $amount;
+        return $amount;
+    }
+
+    /**
+     * @param string $currencyCode
+     * @return mixed
+     * Показать баланс по конкретной валюте или суммарный по основной валюте
+     */
+    public function getBalance(string $currencyCode = ''): mixed
+    {
+        if ($currencyCode === '') {
+            $currencyCode = $this->mainCurrency->getCode();
+            if (!isset($this->balances[$currencyCode])) {
+                return false;
+            }
+            $sum = $this->balances[$currencyCode];
+            foreach ($this->currencies as $code => $currency) {
+                if ($code === $currencyCode) {
+                    continue;
+                }
+                $sum += $currency->convertTo($this->balances[$code], $currencyCode);
+            }
+            return $sum;
+        }
+        if (!isset($this->balances[$currencyCode])) {
+            return false;
+        }
+        return $this->balances[$currencyCode];
+    }
+
+    /**
+     * @param string $fromCurrencyCode
+     * @param string $toCurrencyCode
+     * @param float $amount
+     * @return bool
+     * Конвертация валюты
+     */
+    public function convertCurrency(string $fromCurrencyCode, string $toCurrencyCode, float $amount): bool
+    {
+        if (!isset($this->balances[$fromCurrencyCode]) || !isset($this->currencies[$toCurrencyCode])) {
+            return false;
+        }
+        if ($this->balances[$fromCurrencyCode] < $amount) {
+            return false;
+        }
+        $fromCurrency = $this->currencies[$fromCurrencyCode];
+        $toCurrency = $this->currencies[$toCurrencyCode];
+        $fromAmount = $this->withdraw($fromCurrencyCode, $amount);
+        $convertedAmount = $fromCurrency->convertTo($fromAmount, $toCurrency);
+        $this->deposit($toCurrencyCode, $convertedAmount);
+
+        return true;
+    }
+
+    /**
+     * @param string $currencyCode
+     * @return bool
+     * Отключение валюты
+     */
+    public function removeCurrency(string $currencyCode): bool
+    {
+        if (!isset($this->balances[$currencyCode])) {
+            return false;
+        }
+        $balance = $this->balances[$currencyCode];
+        $balance = $this->withdraw($currencyCode, $balance);
+        $currency = $this->currencies[$currencyCode];
+        $this->deposit($this->mainCurrency->getCode(), $currency->convertTo($balance, $this->mainCurrency->getCode()));
+        unset($this->balances[$currencyCode]);
+        unset($this->currencies[$currencyCode]);
+
+        return true;
+    }
 }
