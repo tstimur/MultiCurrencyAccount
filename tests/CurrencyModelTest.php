dbf49124081fca1b300a2500ca18a821d95cbf41
tstimur <tstimurb0311@gmail.com>
2024-07-17 04:13:18 +0700

    add: class CurrencyModelTest     Создание класса и директории для тестирования класса бизнес логики находящаяся в CurrencyModel

---
+++ tests/CurrencyModelTest.php
@@ -0,0 +1,33 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class CurrencyModelTest extends TestCase
+{
+
+    public function testConvertTo()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testGetExchangeRate()
+    {
+
+    }
+
+    public function testSetExchangeRate()
+    {
+
+    }
+
+    public function testGetCode()
+    {
+
+    }
+}
